368e782e7c9e060e57a045ad142ea20f35ac1822
Biagio Scaglia <scagliabiagio99@gmail.com>
2026-09-04 13:01:32 +0200

    chore(release): v0.1.18

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -10,7 +10,7 @@ import { HtmlRenderer } from '@yumiamd/renderer-html';
 import { PdfRenderer } from '@yumiamd/renderer-pdf';
 import { startDevServer } from './dev-server.js';
 
-export const VERSION = '0.1.17';
+export const VERSION = '0.1.18';
 
 export function printHelp(): string {
   return `
